d4e888b5bda9e8b16c62a76f22abaced1fac1e55
Lucas Kalil <lucas.prokalil2020@outlook.com>
2025-05-13 14:38:14 -0300

    workout revisado

--- lib/widgets/Pages/workout/workout_page.dart
+++ lib/widgets/Pages/workout/workout_page.dart
@@ -3,7 +3,6 @@ import 'package:fittrackr/states/metadata_state.dart';
 import 'package:fittrackr/states/training_plan_state.dart';
 import 'package:fittrackr/widgets/Pages/workout/workout_widgets.dart';
 import 'package:fittrackr/widgets/common/default_widgets.dart';
-import 'package:fittrackr/widgets/Pages/workout/training_plan_widget.dart';
 import 'package:fittrackr/widgets/forms/training_plan_form.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
