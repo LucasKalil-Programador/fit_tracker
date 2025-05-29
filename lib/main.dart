4015341bc92551e543e35633a13ddd766a8247e5
Lucas Kalil <lucas.prokalil2020@outlook.com>
2025-05-29 18:18:48 -0300

    adaptado o BaseListState para usar o DatabaseProxy part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,22 @@
 import 'package:fittrackr/app.dart';
-import 'package:fittrackr/database/save_load_utils/load_utils.dart';
-import 'package:fittrackr/database/save_load_utils/save_utils.dart';
+
+import 'package:fittrackr/database/db.dart';
 import 'package:fittrackr/states/app_states.dart';
 import 'package:fittrackr/states/metadata_state.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  final db = await DatabaseProxy.instance;
+ 
   // instantiates States 
-  final trainingPlanState = TrainingPlanState();
-  final exercisesState    = ExercisesState();
+  final trainingPlanState = TrainingPlanState(db.trainingPlan);
+  final exercisesState    = ExercisesState(db.exercise);
   final metadataState     = MetadataState();
-  final reportTableState  = ReportTableState();
-  final reportState       = ReportState();
-
-  // Try load each database and setup saver callback
-  await loadDatabase(metadataState, exercisesState, trainingPlanState, reportTableState, reportState);
-
+  final reportTableState  = ReportTableState(null);
+  final reportState       = ReportState(null);
+  
   runApp(
     MultiProvider(
       providers: [
@@ -32,44 +29,4 @@ void main() async {
       child: App(),
     ),
   );
-}
-
-Future<void> loadDatabase(
-  MetadataState metadataState,
-  ExercisesState exercisesState,
-  TrainingPlanState trainingPlanState,
-  ReportTableState reportTableState,
-  ReportState reportState) async {
-  late DateTime start;
-  if(kDebugMode) start = DateTime.now();
-  
-  final loadtask1 = loadMetadata().then(metadataState.setMap);
-  final loadtask2 = loadExercises().then(exercisesState.setList);
-  final loadtask3 = loadTrainingPlans().then(trainingPlanState.setList);
-  await Future.wait([loadtask1, loadtask2, loadtask3])
-    .then((value) {
-      // setup call backs to save data in database
-      setupSaver(metadataState, exercisesState, trainingPlanState); 
-    });
-
-  if(kDebugMode) {
-    final elapsed = DateTime.now().difference(start);
-    print("Load took: $elapsed");
-  }
-}
-
-void setupSaver(MetadataState metadataState, ExercisesState exercisesState, TrainingPlanState trainingPlanState) {
-  final debounceSaver = DebounceRunner(
-    delay: Duration(seconds: 1),
-    callback: () async {
-      final task1 = saveMetadata(metadataState.clone());
-      final task2 = saveExercise(exercisesState.flushUpdatePatch());
-      final task3 = saveTrainingPlan(trainingPlanState.flushUpdatePatch());
-      await Future.wait([task1, task2, task3]);
-    }
-  );
-  
-  metadataState.addListener(debounceSaver.runAfter);
-  exercisesState.addListener(debounceSaver.runAfter);
-  trainingPlanState.addListener(debounceSaver.runAfter);
 }
